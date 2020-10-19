a38e8b6a74fb05d720f15f748871a9253e97e0c8
usernamenotfound <elinero27@gmail.com>
2020-10-19 15:45:07 -0400

    Testing version list stuff

---
+++ docs/scripts/showhide.js
@@ -0,0 +1,8 @@
+function toggleshowhide() {
+    var x = document.getElementById("showhideDiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
